723a99d6f7050756f13f27e6c592a7861cd3502f
Amendoza20 <20.angel.mendoza@gmail.com>
2020-05-04 19:26:37 -0400

    still playing with react

---
+++ src/Model.js
@@ -0,0 +1,44 @@
+
+const list = [
+    {
+      id:1,
+      title: 'Fallout',
+      year: 1997,
+      stars: 4,
+      url: 'https://en.wikipedia.org/wiki/Fallout_(series)#Fallout_(1997)',
+    },
+  
+    {
+      id:2,
+      title: 'Fallout 2',
+      year: 1998,
+      stars: 5,
+      url: 'https://en.wikipedia.org/wiki/Fallout_(series)#Fallout_2_(1998)'
+    },
+  
+    {
+      id:3,
+      title: 'Fallout 3',
+      year: 2008,
+      stars: 4,
+      url: 'https://en.wikipedia.org/wiki/Fallout_(series)#Fallout_3_(2008)'
+    },
+       
+    {
+      id:4,
+      title: 'Fallout: New Vegas',
+      year: 2010,
+      stars: 5,
+      url:'https://en.wikipedia.org/wiki/Fallout_(series)#Fallout:_New_Vegas_(2010)'
+    },
+  
+    {
+      id:5,
+      title: 'Fallout 4',
+      year: 2015,
+      stars: 4,
+      url:'https://en.wikipedia.org/wiki/Fallout_(series)#Fallout_4_(2015)',
+    }
+  ];
+
+  export default Model;
